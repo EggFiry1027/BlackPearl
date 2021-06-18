c36d40111e8a26940df11ae7d056edd274412410
FireFighter1027 <ipswich710@gmail.com>
2021-06-18 09:19:50 +0530

    Fixed Cmd Execution

--- start.py
+++ start.py
@@ -1,4 +1,3 @@
-import subprocess, os
-cmd = f"cd .{os.sep}BlackPearl-7788{os.sep}"
-subprocess.run(cmd, shell=True)
-subprocess.run("python run.py", shell=True)
+import  os
+os.system(f"cd .{os.sep}BlackPearl-7788{os.sep}")
+os.system("python run.py")
