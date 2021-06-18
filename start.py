a4acb52ae25b78599b0326e004565052c3c89844
FireFighter1027 <ipswich710@gmail.com>
2021-06-18 08:59:25 +0530

    Fixed Execution of Bot

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 import subprocess, os
 cmd = f"cd .{os.sep}BlackPearl-7788{os.sep}"
 subprocess.run(cmd, shell=True)
-subprocess.run("py -3 run.py", shell=True)
+subprocess.run("python run.py", shell=True)
