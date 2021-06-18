cd9ab0b873c3be3b7267cddeb612485af0c6b839
FireFighter1027 <ipswich710@gmail.com>
2021-06-18 09:53:34 +0530

    Moved All Files To Root Directory

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python start.py
+web: python run.py
--- start.py
+++
@@ -1,3 +0,0 @@
-import  os
-os.system(f"cd {os.getcwd()}BlackPearl-7788{os.sep}")
-os.system(f"python {os.getcwd()}BlackPearl-7788{os.sep}run.py")
